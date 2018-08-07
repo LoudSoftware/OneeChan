d0cef5f5da9098d9bb16ab9c50d69384fea56fd9
LoudSoftware <contact@loud.software>
2018-08-07 13:14:18 -0400

    Automatically creating channel when user is playing a game and moving the user to said channel

--- src/Commands/Util/setup.ts
+++ src/Commands/Util/setup.ts
@@ -1,12 +1,10 @@
-import { Message } from "discord.js";
-import { ArgumentInfo, Command, CommandoClient, CommandMessage } from "discord.js-commando";
+import { Message, CategoryChannel, Channel, GuildChannel } from "discord.js";
+import { ArgumentInfo, Command, CommandoClient, CommandMessage, ArgumentType } from "discord.js-commando";
 
 export default class extends Command {
 
-    private arguments: ArgumentInfo[];
-
-
     constructor(client: CommandoClient) {
+
         super(client, {
             name: 'set',
             group: 'util',
@@ -16,7 +14,11 @@ export default class extends Command {
                 {
                     key: 'option',
                     prompt: 'CategoryChannel or VoiceChannel?',
-                    type: 'string'
+                    type: 'string',
+                    validate: (text: string) => {
+                        if (text.toLowerCase().includes('category')) return true;
+                        if (text.toLowerCase().includes('channel')) return true;
+                    }
                 },
                 {
                     key: 'value',
@@ -25,12 +27,26 @@ export default class extends Command {
                 }
             ],
             argsCount: 2,
-            argsSingleQuotes: true
+            argsSingleQuotes: true,
+            guildOnly: true
         });
     }
 
-    public async run(message: CommandMessage, args: {option: string, value: string}): Promise<Message | Message[]> {
-        return message.channel.send(`Category channel name will be set to: ${args.value}`);
+    public async run(message: CommandMessage, args: { option: string, value: string }): Promise<Message | Message[]> {
+        if (args.option.toLowerCase().includes('category')) {
+            const category: Channel = await message.guild.createChannel(args.value, 'category');
+            await this.client.provider.set(message.guild, 'categoryChannel', category.id);
+            return message.channel.send(`Category channel name will be set to: ${args.value}`);
+        }
+        if (args.option.toLowerCase().includes('channel')) {
+            const categoryId = this.client.provider.get(message.guild, 'categoryChannel');
+            if (categoryId === undefined) return message.reply('You must set a category channel first');
+            const categoryChannel: GuildChannel = message.guild.channels.get(categoryId);
+            const voiceChannel = await message.guild.createChannel(args.value, 'voice');
+            await this.client.provider.set(message.guild, 'voiceChannel', voiceChannel.id);
+            await voiceChannel.setParent(categoryChannel);
+            return message.channel.send(`Voice channel name will be set to: ${args.value}`);
+        }
     }
 
 }
--- src/Util/Listeners.ts
+++ src/Util/Listeners.ts
@@ -1,20 +1,42 @@
 import { GuildMember } from "discord.js";
+import { CommandoClient } from "discord.js-commando";
 import { _logger } from "./logger";
 
 export class Listeners {
+    private client: CommandoClient;
 
-    public static _onVoiceStateUpdate(old: GuildMember, current: GuildMember): void {
+    constructor(client: CommandoClient){
+        this.client = client;
+    }
+
+    public async _onVoiceStateUpdate(old: GuildMember, current: GuildMember): Promise<any> {
         // Voice Channel id changed, user either left or changed to another voice channel.
         if (old.voiceChannelID !== current.voiceChannelID) {
             // He switched
             // TODO figure out if he's in the right channel
             if (current.voiceChannelID) {
                 _logger.info(`${current.user.username} joined ${current.voiceChannel.name}`);
+                const voiceID = await this.client.provider.get(current.guild, 'voiceChannel');
+                if (current.voiceChannelID === voiceID) {
+                    await this.createAutoChannel(current);
+                }
 
             } else {
                 // He left
                 _logger.info(`${current.user.username} left ${old.voiceChannel.name}`);
             }
+        }
+    }
+
+    private async createAutoChannel(member: GuildMember): Promise<any> {
+        if (member.presence.game) {
+            const categoryID = this.client.provider.get(member.guild, 'categoryChannel');
+            _logger.info(member.presence.game.name);
+
+            const gameChannel = await member.guild.createChannel(member.presence.game.name, 'voice');
+            gameChannel.setParent(categoryID);
+            member.setVoiceChannel(gameChannel);
+
         }
     }
 }
